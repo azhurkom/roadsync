f265186e7de4674708b59f8a2f18c03adaca8b25
kom18 <azhurkom@gmail.com>
2026-03-15 13:58:30 +0100

    add service worker

--- public/sw.js
+++ public/sw.js
@@ -1,25 +1,5 @@
 const CACHE_NAME = 'roadsync-v1';
 const STATIC_ASSETS = ['/', '/manifest.json', '/icon-192x192.png', '/icon-512x512.png'];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-  const url = new URL(event.request.url);
-  if (url.pathname.startsWith('/api/')) return;
-  event.respondWith(
-    fetch(event.request).then((response) => {
-      const clone = response.clone();
-      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
-      return response;
-    }).catch(() => caches.match(event.request))
-  );
-});
+self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(STATIC_ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((k) => Promise.all(k.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n))))); self.clients.claim(); });
+self.addEventListener('fetch', (e) => { if (e.request.method !== 'GET') return; const u = new URL(e.request.url); if (u.pathname.startsWith('/api/')) return; e.respondWith(fetch(e.request).then((r) => { caches.open(CACHE_NAME).then((c) => c.put(e.request, r.clone())); return r; }).catch(() => caches.match(e.request))); });
